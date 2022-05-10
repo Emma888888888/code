fdb85a5904b46c048054b57dc5ca7013d6a35924
Emma888888888 <103553698+Emma888888888@users.noreply.github.com>
2022-05-11 00:01:05 +1000

    class7 commit

---
+++ code2.py
@@ -0,0 +1,65 @@
+# print(5%2) #取余数
+# print(5**2)#平方
+# print(5//2)
+
+# #赋值运算符
+# #累加运算符 x=x+3  =  x+=3
+
+# x=1
+# x=x+1
+# print(x)
+
+# y=1
+# y+=1
+# print(y)
+ 
+# #basic operators - comparison operators
+# print (2 == 2)
+# #boolean True or False
+# print(2!=2)
+# print(2>1)
+# print('C'<'B')
+# print('a'<'b')
+# print('C'<'d') #in ASKII code, C is 67, d is 100, 十进制数字
+# print('F'<'a')#F is 70, a is 97
+# print("Tim"<'Tom') #先比T，再比i 105，o 111
+# #basic operators: logical operators
+# #and: 一假即假（串联电路图）
+# print(3>2 and 2>3)
+#or: 一真即真（并联电路图）
+# print(3>2 or 2>3)
+# print('true'>"false")
+#not: 取反值
+# print(not 3>2 and 2>3)
+# print(not(3>2 and 2>3))
+
+#identity operators: is , is not
+#membership operators: in/not in
+# name='John'
+# if name in ['John','Rick']:
+    # print('Your name is either John or Rick')
+
+
+
+
+# name='Apple'
+# if name in ['John','Rick']:
+#     print('Your name is Neither John nor Rick')
+
+# print('Always')
+
+
+# fruits=['apple','orange','other','orange']
+# fruits.remove('orange')
+# print(fruits)
+
+# person={"age":18,"name":"ollie"}
+# print(person)
+
+from tkinter import E
+
+
+nums={9,1,12.6,78,5}
+# empty_set={}
+empty_set=set()
+print(type(empty_set))
