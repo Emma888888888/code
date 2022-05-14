d249333bd4fdc488051ef768038a20e38696f482
Emma888888888 <103553698+Emma888888888@users.noreply.github.com>
2022-05-15 02:06:39 +1000

    class 8 commit

---
+++ class8classnotes.py
@@ -0,0 +1,48 @@
+#for loop
+
+# for num in range(2,10):
+#     if num % 2 == 0:
+#         print("Found an even number", num)
+#     else:
+#         print("Found an number", num)
+
+#pass statement
+# for num in range(2,10):
+#     pass
+# print(123)
+
+#break statement
+# for num in range(2,10):
+#     break    
+# print(num)
+
+#continue statement
+# for num in range(2,10):
+#     if num != 8:
+#         print("This is not a 8,", num) 
+#     else: 
+#         continue
+#         print("This is a 8,", num)
+#     print("print num", num)
+
+#Nested for loop: 找inter-section
+#Q: Exercise: search for the same letter within two strings
+#eg: a='python', b='excellent'. Try to find out the same letter 
+#within two strings and print out only the same letters
+
+# for x in ('python'):
+#     for y in ('excellent'):
+#         if x == y:
+#             print(x,y)
+
+"""
+Parameters & arguments
+"""
+
+# def add(a,b):
+#     inner_result=a+b
+#     print(inner_result)
+
+# result=add(1,2)
+# print(result)
+
